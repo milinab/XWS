d9b7e2a100e009d2ecda575681ed23cfd8a79165
Aleksa Ignjatovic <aleksaignjatovic15@gmail.com>
2023-03-21 18:36:16 +0100

    controller, service init

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -22,10 +22,12 @@ public class DemoApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(FlightRepository repository) {
+	CommandLineRunner runner(FlightRepository repository, AppUserRepository appUserRepository) {
 		return args -> {
 			Flight flight = new Flight(250);
+			AppUser appUser = new AppUser("kia", "Aleksa", "Ignjatovic", "aleksaignjatovic20@gmail.com", "123");
 			repository.insert(flight);
+			appUserRepository.insert(appUser);
 		};
 
 	}
