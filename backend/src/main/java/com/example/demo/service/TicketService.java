d6a8a95b2305612dd7a8a7fd4fc80401685f2356
Nemanja2106 <nemkejelic15@gmail.com>
2023-03-29 22:04:38 +0200

    create multiple tickets

--- backend/src/main/java/com/example/demo/service/TicketService.java
+++ backend/src/main/java/com/example/demo/service/TicketService.java
@@ -2,9 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.model.Ticket;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface TicketService {
     Ticket save(Ticket ticket);
     List<Ticket> findAll();
+    List<Ticket> createMultipleTicket(List<Ticket> ticket);
 }
