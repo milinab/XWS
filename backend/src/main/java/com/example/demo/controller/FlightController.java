d0ff5b3b81eb4fc7a2faec41be7284f825f26e61
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2023-03-29 17:25:57 +0200

    flight methods

--- backend/src/main/java/com/example/demo/controller/FlightController.java
+++ backend/src/main/java/com/example/demo/controller/FlightController.java
@@ -1,4 +1,32 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.FlightDTO;
+import com.example.demo.model.Flight;
+import com.example.demo.service.FlightService;
+import com.example.demo.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(value = "api/flights")
+
 public class FlightController {
+    private FlightService flightService;
+    @Autowired
+    public FlightController(FlightService flightService){this.flightService = flightService;}
+
+    @GetMapping (value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<FlightDTO> getFlight(@PathVariable("id") String id){
+        Flight flight = flightService.findOne(id);
+        if(flight == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        FlightDTO flightDTO = new FlightDTO(flight);
+        return new ResponseEntity<>(flightDTO, HttpStatus.OK);
+    }
 }
+
--- backend/src/main/java/com/example/demo/service/FlightServiceImpl.java
+++ backend/src/main/java/com/example/demo/service/FlightServiceImpl.java
@@ -1,4 +1,27 @@
 package com.example.demo.service;
 
-public class FlightServiceImpl {
+import com.example.demo.model.Flight;
+import com.example.demo.repository.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FlightServiceImpl implements FlightService{
+    private FlightRepository flightRepository;
+
+    @Autowired
+    public FlightServiceImpl(FlightRepository flightRepository) {this.flightRepository  = flightRepository;}
+
+    @Override
+    public Flight findOne(String id) {
+        Optional<Flight> flight = flightRepository.findById(id);
+        if(flight.isPresent()){
+            return flight.get();
+        }
+        return null;
+    }
+
+
 }
