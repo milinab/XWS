8c5c449a23ccc17769520e7da93b54b87f6f4548
Nemanja2106 <nemkejelic15@gmail.com>
2023-05-10 00:51:49 +0200

    initialized customer microservice and implemented login and registration

---
+++ customer/src/main/java/com/example/customer/model/User.java
@@ -0,0 +1,56 @@
+package com.example.customer.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Document(collection = "users")
+@Builder
+public class User implements UserDetails {
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    @NonNull
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
+    @NonNull
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
